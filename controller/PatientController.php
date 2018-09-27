f5262eef3f357f39cc7a24a2ff7939172e250136
Conde Martínez, Matías <matiascondemartinez1997@gmail.com>
2018-09-27 14:27:21 -0300

    bulma y archivos del paciente

---
+++ controller/PatientController.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once('model/PacientRepository.php');
+
+/**
+ * Controlador del usuario
+ */
+class UserControler extends MainController{
+  protected static $instance;
+  protected static $twig;
+
+
+
+}
+
+?>
---
+++ model/PatientRepository.php
@@ -0,0 +1,26 @@
+<?php
+require_once('core/Connection.php');
+  /**
+   * Repositorio de usuario
+   */
+  class PatientRepository extends Connection{
+
+    function __construct(){
+      parent::__construct();
+    }
+
+    /* Create functions */
+    /* End of create functions */
+
+    /* READ functions */
+    /* End of read  functions */
+
+    /* Update functions */
+    /* End of update functions */
+
+    /* Delete functions */
+    /* End of delete functions */
+  }
+
+
+?>
