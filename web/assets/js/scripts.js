59624afb3a370661858bdb518f296c6ebeea0ef5
Conde Martínez, Matías <matiascondemartinez1997@gmail.com>
2018-10-01 11:57:12 -0300

    jquery bien linkeado

---
+++ web/assets/js/scripts.js
@@ -0,0 +1,53 @@
+
+          $(document).ready(function() {
+            var url="https://api-referencias.proyecto2018.linti.unlp.edu.ar/partido";
+            $.getJSON(url, function(data) {
+              $("#partido option").remove();
+              $("#partido").append('<option value="">Selecciona un partido</option>');
+              $.each(data,function(index, value){
+                $("#partido").append(
+                  '<option value="' + value.id + '">' + value.nombre + "</option>"
+                );
+              });
+            });
+
+            $("#partido").on("change",function(){
+              var partido=$("#partido").val();
+              if(!$.isEmptyObject(partido)){ //esto es para evitar que pida el valor nulo al seleccionar el "selecciona un partido"
+              var url="https://api-referencias.proyecto2018.linti.unlp.edu.ar/region-sanitaria/"+partido;
+              $.getJSON(url, function(data){
+                $("#region_s").val(data.nombre);
+              });
+            }
+            else{
+              $("#region_s").val("");
+            }
+            });
+
+            $("#partido").on("change",function(){
+              var partido_id=$("#partido").val();
+              if(!$.isEmptyObject(partido_id)){ //esto es para evitar que pida el valor nulo al seleccionar el "selecciona un partido"
+              var url="https://api-referencias.proyecto2018.linti.unlp.edu.ar/localidad/partido/"+partido_id;
+              $.getJSON(url, function(data) {
+                $("#localidad option").remove();
+                $("#localidad").append('<option value="">Selecciona una localidad</option>');
+                if(!$.isArray(data)){
+                  $("#localidad").append(
+                  '<option value="' + data.id + '">' + data.nombre + "</option>"
+                  );
+                }
+                else{
+                $.each(data,function(index, value){
+                  $("#localidad").append(
+                    '<option value="' + value.id + '">' + value.nombre + "</option>"
+                  );
+                });
+              } //cierre del else
+              });
+            }
+            else{
+              $("#localidad option").remove();
+              $("#localidad").append('<option value="">Selecciona una localidad</option>');
+            }
+            });
+          });
