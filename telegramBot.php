83562a81153733bd047a3f4b94b338172b51dee7
Conde Martínez, Matías <matiascondemartinez1997@gmail.com>
2018-11-16 11:42:17 -0300

    decimoseptima prueba api

--- telegramBot.php
+++ telegramBot.php
@@ -1,8 +1,10 @@
 <?php
+
 $botToken="763852469:AAFUJhgWC4xoGEw1DXE3cKPXpROwa5H7V1U";
 $website="https://api.telegram.org.bot".$botToken;
 
-$update=json_decode(file_get_contents('php://input'),TRUE);
+$update=file_get_contents('php://input');
+$update=json_decode($update,TRUE);
 
 $chatId=$update['message']['chat']['id'];
 
@@ -19,7 +21,6 @@ switch ($message) {
     break;
 }
 
-}
 function sendMessage($chatId,$message){
 $url=$GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
 file_get_contents($url);
