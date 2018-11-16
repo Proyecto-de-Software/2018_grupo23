098db6599955d0276aa29b7c6c4707cc00a709e0
Conde Martínez, Matías <matiascondemartinez1997@gmail.com>
2018-11-16 10:23:40 -0300

    septima prueba api

--- controller/APIController.php
+++ controller/APIController.php
@@ -10,29 +10,106 @@ class APIController extends MainController{
   protected static $twig;
 
   function api(){
-    $botToken="763852469:AAFUJhgWC4xoGEw1DXE3cKPXpROwa5H7V1U";
-    $website="https://api.telegram.org.bot".$botToken;
+    $returnArray = true;
+    $rawData = file_get_contents('php://input');
+    $response = json_decode($rawData, $returnArray);
+    $chatId = $response['message']['chat']['id'];
 
-    $update=json_decode(file_get_contents('php://input'),TRUE);
 
-    $chatId=$update['message']['chat']['id'];
+    // Obtener comando (y sus posibles parametros)
+    $regExp = '#^(\/[a-zA-Z0-9\/]+?)(\ .*?)$#i';
 
-    $message=$update['message']['text'];
 
-    switch ($message) {
-      case '/ayuda':
-        $rensponse='yo te ayudo np';
-        sendMessage($chatId,$rensponse);
-        break;
+    $tmp = preg_match($regExp, $response['message']['text'], $aResults);
 
-      default:
-        // code...
-        break;
+    if (isset($aResults[1])) {
+        $cmd = trim($aResults[1]);
+        $cmd_params = trim($aResults[2]);
+    } else {
+        $cmd = trim($response['message']['text']);
+        $cmd_params = '';
     }
+
+    $msg = array();
+    $msg['chat_id'] = $response['message']['chat']['id'];
+    $msg['text'] = null;
+    $msg['disable_web_page_preview'] = true;
+    $msg['reply_to_message_id'] = $response['message']['message_id'];
+    $msg['reply_markup'] = null;
+
+    switch ($cmd) {
+    case '/start':
+        $msg['text']  = 'Hola ' . $response['message']['from']['first_name'] .
+                   " Usuario: " . $response['message']['from']['username'] . '!' . PHP_EOL;
+        $msg['text'] .= '¿Como puedo ayudarte? /help';
+        $msg['reply_to_message_id'] = null;
+        break;
+
+    case '/help':
+        $msg['text']  = 'Los comandos disponibles son estos:' . PHP_EOL;
+        $msg['text'] .= '/start Inicializa el bot' . PHP_EOL;
+        $msg['text'] .= '/turnos dd-mm-aaaa Muestra los turnos disponibles del día' . PHP_EOL;
+        $msg['text'] .= '/reservar dd-mm-aaaa hh:mm Realiza la reserva del turno' . PHP_EOL;
+        $msg['text'] .= '/help Muestra esta ayuda flaca';
+        $msg['reply_to_message_id'] = null;
+        break;
+
+    case '/reservar':
+        $msg['text']  = 'Te confirmamos el turno para:' . PHP_EOL;
+        $msg['text'] .= '10:30' . PHP_EOL;
+        $msg['reply_to_message_id'] = null;
+        break;
+
+    case '/turnos':
+        $msg['text']  = 'Los turnos disponibles son: 10:30 | 11:45 | 15:15';
+        break;
+
+    default:
+            $msg['text']  = 'Lo siento, no es un comando válido.' . PHP_EOL;
+            $msg['text'] .= 'Prueba /help para ver la lista de comandos disponibles';
+            break;
+    }
+
+    $url = 'https://api.telegram.org/bot763852469:AAFUJhgWC4xoGEw1DXE3cKPXpROwa5H7V1U/sendMessage';
+
+    $options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($msg)
+        )
+    );
+
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+
+    exit(0);
+  }
+}
+
+
+/* $botToken="763852469:AAFUJhgWC4xoGEw1DXE3cKPXpROwa5H7V1U";
+$website="https://api.telegram.org.bot".$botToken;
+
+$update=json_decode(file_get_contents('php://input'),TRUE);
+
+$chatId=$update['message']['chat']['id'];
+
+$message=$update['message']['text'];
+
+switch ($message) {
+  case '/ayuda':
+    $rensponse='yo te ayudo np';
+    sendMessage($chatId,$rensponse);
+    break;
+
+  default:
+    // code...
+    break;
+}
 
 }
 function sendMessage($chatId,$message){
-  $url=$GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
-  file_get_contents($url);
-}
-}
+$url=$GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+file_get_contents($url);
+} */
