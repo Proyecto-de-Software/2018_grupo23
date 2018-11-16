8a65ea87fea8a8aeaea94c5eef38fc913030c39e
Conde Martínez, Matías <matiascondemartinez1997@gmail.com>
2018-11-16 10:18:26 -0300

    sexta prueba api

--- controller/APIController.php
+++ controller/APIController.php
@@ -10,7 +10,29 @@ class APIController extends MainController{
   protected static $twig;
 
   function api(){
-    echo 'prueba';
-    die();
-  }
+    $botToken="763852469:AAFUJhgWC4xoGEw1DXE3cKPXpROwa5H7V1U";
+    $website="https://api.telegram.org.bot".$botToken;
+
+    $update=json_decode(file_get_contents('php://input'),TRUE);
+
+    $chatId=$update['message']['chat']['id'];
+
+    $message=$update['message']['text'];
+
+    switch ($message) {
+      case '/ayuda':
+        $rensponse='yo te ayudo np';
+        sendMessage($chatId,$rensponse);
+        break;
+
+      default:
+        // code...
+        break;
+    }
+
+}
+function sendMessage($chatId,$message){
+  $url=$GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+  file_get_contents($url);
+}
 }
