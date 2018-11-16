9dec563a4ec5aa9c9dec5c88a7b15b6abb2360f2
Conde Martínez, Matías <matiascondemartinez1997@gmail.com>
2018-11-16 11:52:58 -0300

    vigesima prueba api

--- telegramBot.php
+++ telegramBot.php
@@ -22,6 +22,6 @@ switch ($message) {
 }
 
 function sendMessage($chatId,$response){
-$url=$GLOBALS[website].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+$url=$GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
 file_get_contents($url);
 }
