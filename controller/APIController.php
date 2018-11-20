74b2d8842d237ebebce58b58b82555a1c0ff2fa0
Conde Martínez, Matías <matiascondemartinez1997@gmail.com>
2018-11-20 09:57:09 -0300

    prueba 8 bot telegram fix

--- controller/APIController.php
+++ controller/APIController.php
@@ -31,7 +31,7 @@ class APIController{
 
   function list_institution_by_RS($id){
     $db=new APIRepository;
-    echo json_encode(empty($db->getInstitutionByRSID($id));
+    echo json_encode(empty($db->getInstitutionByRSID($id)));
   }
 
 
